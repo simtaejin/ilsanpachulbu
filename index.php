73170ca3362c7c49b264ddd5a42a762142f36117
simtaejin <stj53@nate.com>
2025-11-25 15:13:27 +0900

    레이아웃 수정 및 불필요한 코드 정리

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
         <section class="contents" style="padding-bottom: 30px;">
             <div class="inner">
 <!--                .price_top {height: 94px;}-->
-                <div class="grid mt_30" style="display: flex; justify-content: center; gap: 20px;">
+                <div class="grid mt_30" style="display: flex; justify-content: center; gap: 60px;">
                     <img src="./images/restaurant_service.png">
                     <img src="./images/housekeeping_service.png">
                 </div>
